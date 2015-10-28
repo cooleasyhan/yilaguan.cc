f874e16480e5cce82f5fcafb19b6c0cb26cbb242
cooleasyhan <yihan.cug@outlook.com>
2015-10-28 09:15:48 -0700

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'YiHan'
+SITENAME = u'YiLaGuan'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('CodeSearch', 'http://www.yilaguan.cc/cs'),
+         ('CodeSearch_IP_IP', 'http://123.56.41.91/cs'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+STATIC_PATHS = ['images', 'pdfs']
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
